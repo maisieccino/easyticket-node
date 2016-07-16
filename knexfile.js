ac65430e370c4abf6a020d8195acaf3fd0b30c51
Matthew Bell <logicnotlogical@outlook.com>
2016-07-16 02:16:48 +0100

    Finalise initial migration

--- knexfile.js
+++ knexfile.js
@@ -4,46 +4,37 @@ var config = require('./config');
 
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-        host: config.db.host,
-        user: config.db.user,
-        password: config.db.pass,
-        database: config.db.dbname
-    }
-  },
+    development: {
+        client: 'postgresql',
+        connection: {
+            host: config.db.host,
+            user: config.db.user,
+            password: config.db.pass,
+            database: config.db.dbname
+        }
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    staging: {
+        client: 'postgresql',
+        connection: {
+            host: config.db.host,
+            user: config.db.user,
+            password: config.db.pass,
+            database: config.db.dbname
+        }
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    production: {
+        client: 'postgresql',
+        connection: {
+            host: config.db.host,
+            user: config.db.user,
+            password: config.db.pass,
+            database: config.db.dbname
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     }
-  }
 
 };
--- migrations/20160715191348_initial.js
+++ migrations/20160715191348_initial.js
@@ -1,7 +1,7 @@
-exports.up = function(knex, Promise) {
-    returns knex.schema
-        .createTable('user', function(table) {
-            table.bigincrements('id').primary();
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('user', function (table) {
+            table.bigIncrements('id').unsigned().primary();
             table.string('name').notNullable();
             table.string('name_short');
             table.string('img_profile_url');
@@ -9,22 +9,70 @@ exports.up = function(knex, Promise) {
             table.string('email').notNullable().unique();
             table.string('password_digest');
         })
-        .createTable('org_user', function(table) {
-            table.bigincrements('id').primary();
-            table.bigInteger('user_id').unique().inTable('user').references('id');
-            table.bigInteger('org_id').unique().inTable('organisations').references('id');
-        })
-        .createTable('event', function(table) {
-            table.bigincrements('id').primary();
+        .createTable('organisation', function (table) {
+            table.bigIncrements('id').unsigned().primary();
             table.string('name').notNullable();
-            table.description('description');
-            table.datetime('datetime_start').notNullable();
-            table.datetime('datetime_end').notNullable();
+            table.string('description');
+            table.string('website');
+            table.string('email');
+            table.string('facebook');
+            table.string('twitter');
             table.string('img_logo_url');
             table.string('img_banner_url');
+        })
+        .createTable('venue', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.string('name').notNullable();
+            table.string('description');
+            table.string('img_banner_url');
+            table.json('address');
+        })
+        .createTable('event', function(table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.string('name').notNullable();
+            table.string('description');
+            table.dateTime('datetime_start').notNullable();
+            table.dateTime('datetime_end').notNullable();
+            table.string('img_logo_url');
+            table.string('img_banner_url');
+            table.bigInteger('venue').unsigned().notNullable().references('id').inTable('venue');
+            table.bigInteger('organisation').unsigned().notNullable().references('id').inTable('organisation');
+        })
+        .createTable('event_category', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.string('name').notNullable();
+            table.string('img_url');
+            table.string('description');
+        })
+        .createTable('event_event_category', function (table) {
+            table.bigInteger('event').unsigned().notNullable();
+            table.bigInteger('event_category').unsigned().notNullable();
+            table.index(['event', 'event_category'], 'event_event_category_index');
+        })
+        .createTable('ticket_tier', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.string('name').notNullable();
+            table.decimal('cost',2).defaultTo(0.00);
+            table.string('description');
+            table.bigInteger('quantity').unsigned().notNullable();
+            table.bigInteger('event').unsigned().notNullable().references('id').inTable('event');
+        })
+        .createTable('ticket', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.string('qr_code_val').unique();
+            table.bigInteger('tier').unsigned().notNullable().references('id').inTable('ticket_tier');
+        })
+        .createTable('org_user', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.bigInteger('user_id').unsigned().references('id').inTable('user');
+            table.bigInteger('org_id').unsigned().references('id').inTable('organisation');
+        })
+        .createTable('admin_user', function (table) {
+            table.bigIncrements('id').unsigned().primary();
+            table.integer('user_id').unsigned().unique().references('id').inTable('user');
         });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
 
 };
